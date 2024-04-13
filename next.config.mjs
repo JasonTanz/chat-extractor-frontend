5a5e54b3c58e53e2bae2a7384d23bc1ca9f6092b
Jasontanz <zisheng0101@gmail.com>
2024-04-13 23:11:10 +0800

    feat: config & package installation

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    env: {
+        API_URL: process.env.NEXT_PUBLIC_API_URL,
+    },
 };
 
 export default nextConfig;
